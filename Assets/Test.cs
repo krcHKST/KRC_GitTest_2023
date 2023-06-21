b4876e2dde1509d49961406509fcf8473f8e6d34
Yamada-Tatsuaki <61475455+Yamada-Tatsuaki@users.noreply.github.com>
2023-06-21 12:27:08 +0900

    ChangeTestScript
    ChangeTestScript

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -7,7 +7,7 @@ public class Test : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Hello there");
+        Debug.Log("Yamada");
     }
 
     // Update is called once per frame
